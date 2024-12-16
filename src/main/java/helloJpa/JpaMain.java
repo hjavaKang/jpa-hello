9f4359686977e0da811be6196a397073209b172c
강현정 <hjkang@etribe.co.kr>
2024-12-16 16:50:45 +0900

    first commit

---
+++ src/main/java/helloJpa/JpaMain.java
@@ -0,0 +1,15 @@
+package helloJpa;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+public class JpaMain {
+    public static void main(String[] args) {
+       EntityManagerFactory emf =  Persistence.createEntityManagerFactory("hello");
+       EntityManager entityManager = emf.createEntityManager();
+
+       entityManager.clear();
+       emf.close();
+    }
+}
